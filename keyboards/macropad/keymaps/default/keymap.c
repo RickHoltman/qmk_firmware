e5831982a0350102cb7f6f6849654f60675bd314
Rick Holtman <info@rickholtman.nl>
2025-04-02 17:15:00 +0200

    probably working config

--- keyboards/macropad/keymaps/default/keymap.c
+++ keyboards/macropad/keymaps/default/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ Z │ X │ C │ V │
      * └───┴───┴───┴───┘
      */
-    [0] = LAYOUT_all(
+    [0] = LAYOUT(
         KC_1,   KC_2,   KC_3,   KC_4,
         KC_5,   KC_6,   KC_7,   KC_8,
         KC_Q,   KC_W,   KC_R,   KC_T,
